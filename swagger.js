02963de4eb3a7e275e4a1139681920e5bfbf79e7
Andrei David Nechitoaia <david@Andreis-MacBook-Pro.local>
2026-02-20 15:14:08 +0100

    Implement API endpoints and add Swagger documentation

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "ECommerce API",
+      version: "1.0.0",
+      description: "REST API for e-commerce project",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/app.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
